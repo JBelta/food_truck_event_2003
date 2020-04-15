488baadb8db09f4159859569df629241dc7b4b70
James Belta <james.belta@gmail.com>
2020-04-15 12:00:37 -0600

    commit to push

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -46,8 +46,8 @@ class FoodTruckTest < Minitest::Test
   end
 
   def test_total_truck_stock
-    def test_add_more_to_stock
       item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
       food_truck = FoodTruck.new("Rocky Mountain Pies")
 
       food_truck.stock(item1, 30)
@@ -57,6 +57,6 @@ class FoodTruckTest < Minitest::Test
       assert_equal 55, food_truck.check_stock(item1)
 
       food_truck.stock(item2, 12)
-      assert_equal ({item1, item2}), food_truck.inventory
+      assert_equal [item1, item2], food_truck.inventory
     end
-end
+  end
