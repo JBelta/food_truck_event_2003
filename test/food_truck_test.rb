975774cdf770329de1d10f6fe3fcebd1640884a2
James Belta <james.belta@gmail.com>
2020-04-15 11:40:02 -0600

    passesd add_to_inventory test

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -28,10 +28,20 @@ class FoodTruckTest < Minitest::Test
 
   def test_add_to_inventory
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     food_truck = FoodTruck.new("Rocky Mountain Pies")
-require 'pry'; binding.pry
+
     food_truck.stock(item1, 30)
     assert_equal 30, food_truck.check_stock(item1)
+  end
+
+  def test_add_more_to_stock
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    food_truck.stock(item1, 30)
+    assert_equal 30, food_truck.check_stock(item1)
+
+    food_truck.stock(item1, 30)
+    assert_equal 55, food_truck.check_stock(item1)
   end
 end
